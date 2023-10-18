9dee84f73ad7767101afac52d334ed38a523d7c3
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-18 18:52:17 -0400

    initial commit

---
+++ src/main/java/com/isomer/springboot/demp/myawesomeapp/MyawesomeappApplication.java
@@ -0,0 +1,13 @@
+package com.isomer.springboot.demp.myawesomeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyawesomeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyawesomeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isomer/springboot/demp/myawesomeapp/MyawesomeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isomer.springboot.demp.myawesomeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyawesomeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
